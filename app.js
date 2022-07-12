98a80f4c0f1090491433faa7a233fb1d8d168e6e
Ahmed Melouk <forprogramming8@gmail.com>
2022-07-12 07:55:47 +0200

    Create playRound function

--- app.js
+++ app.js
@@ -10,3 +10,35 @@ function computerPlay() {
        
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerPlay === "paper") {
+            return "You Lose! Paper beats Rock";
+        }
+        else if (computerPlay === "scisscors") {
+            return "You wind! Rock beats scisscors";
+        } else {
+            return "no one won you both rock"
+        }
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerPlay === "paper") {
+            return "no one won you both rock"
+        }
+        else if (computerPlay === "scisscors") {
+            return "You Lose! scisscors beats paper";
+        } else {
+            return "You wind! paper beats rock";
+        }
+    } else { //scisscors
+        if (computerPlay === "paper") {
+            return "You wind! scissors beats paper"
+        }
+        else if (computerPlay === "scisscors") {
+            return "no one won you both rock";
+        } else {
+            return "You Lose! rock beats scisscors";
+        }
+    }
+}
