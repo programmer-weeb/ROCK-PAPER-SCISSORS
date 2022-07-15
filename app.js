b2e45ed59a62bfc9438693cf54244f6792ab61a3
Ahmed Melouk <forprogramming8@gmail.com>
2022-07-15 09:04:14 +0200

    Fix addEventListener

--- app.js
+++ app.js
@@ -14,47 +14,82 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     // computerSelection = computerPlay();
     if (computerSelection === playerSelection.toLowerCase()) {
-        console.log("Tie game!");
+//         console.log(`you choose ${playerSelection}
+// computer choose ${computerSelection}`);
+        return (
+            `you choose ${playerSelection}
+computer choose ${computerSelection}
+"Tie game!"`
+        )
+        
+        // console.log("Tie game!");
+
     }
     if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection === "paper") {
             return "You Lose! Paper beats Rock";
+            // console.log("You Lose! Paper beats Rock");
         }
         else if (computerSelection === "scisscors") {
             return "You wind! Rock beats scisscors";
+            // console.log("You wind! Rock beats scisscors");
         }
     }
     else if (playerSelection.toLowerCase() === "paper") {
         if (computerSelection === "rock") {
-            return "you won paper beats rock"
+            return "you won paper beats rock";
+            // console.log("you won paper beats rock");
         }
         else if (computerSelection === "scisscors") {
             return "You Lose! scisscors beats paper";
+            // console.log("You Lose! scisscors beats paper");
         }
     } else if (playerSelection.toLowerCase() === "scisscors") { //scisscors
         if (computerSelection === "paper") {
-            return "You wind! scissors beats paper"
+            return "You wind! scissors beats paper";
+            // console.log("You wind! scissors beats paper");
         }
         else if (computerSelection === "rock") {
             return "you lose rock beats scisscors";
+            // console.log("you lose rock beats scisscors");
         }
     }
     else {
-        return 'invalide selection';
+        // return 'invalide selection';
+        return
+        // console.log('invalide selection');
     }
 }
-// const playerSelection = 'rock'
-// const computerSelection = computerPlay()
 
+function game(playerSelection) {
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("what's your selection?");
-        let computerSelection = computerPlay();
-        console.log(`your selection is ${playerSelection}
+    let computerSelection = computerPlay();
+    console.log(`your selection is ${playerSelection}
 computer selection is ${computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
-    }
+    console.log(playRound(playerSelection, computerSelection));
+    // return playRound(playerSelection, computerSelection);
 }
 
-game();
+// DOM manipulation
+
+/* let buttons = document.querySelectorAll('button');
+buttons.forEach((btn) => {
+    btn.addEventListener('click', game(`${btn.id}`))
+}) */
+
+let rockButton = document.querySelector("#rock")
+let paperButton = document.querySelector("#paper")
+let scissorsButton = document.querySelector("#scissors")
+
+rockButton.addEventListener('click', () => {
+    game("rock")
+})
+paperButton.addEventListener('click', () => {
+    game("paper")
+});
+scissorsButton.addEventListener('click', () => {
+    game("scissors")
+});
+
+
+
